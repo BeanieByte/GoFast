1330062dbc55315d00ae95f96dc4d018fb6d685c
Freddy <fredericombgalvao@gmail.com>
2023-05-14 22:43:05 +0100

    Added Game Input
    Need to start on Player Script

---
+++ Assets/InputReader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.InputSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class InputReader : MonoBehaviour
+{
+    [SerializeField] private OnInputSystemEvent gameInput;
+}
